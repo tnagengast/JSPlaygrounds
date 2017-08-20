08d84142ba80a9a32db6ca10cc4603214da8d4a7
Thomas Nagengast <tnagengast@gmail.com>
2017-08-20 13:30:52 -0700

    Change webpack-dev-server to :3000 and add external packages

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,20 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import axios from 'axios';
+import collect from 'collect.js';
+import crypto from 'crypto-js';
+import immutable from 'immutable';
+import moment from 'moment';
+import vue from 'vue';
 
-_.extend(window, { _, React, Component });
+_.extend(window, {
+	_,
+	React,
+	Component,
+	axios,
+	collect,
+	crypto,
+	immutable,
+	moment,
+	vue,
+});
